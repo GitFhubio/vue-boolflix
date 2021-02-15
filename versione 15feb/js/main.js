12345e0c5ad1c24204527842aaf66a734e8b09ad
Fabio Petrone <fa610movic90@gmail.com>
2021-02-15 14:12:09 +0100

    nitidezza immagini

--- versione 15feb/js/main.js
+++ versione 15feb/js/main.js
@@ -76,7 +76,7 @@ this.RicercaSerie();
         // per mettere locandina
         // https://www.themoviedb.org/talk/568e3711c3a36858fc002384
          let uri= 'https://image.tmdb.org/t/p/';
-         let size='w342';
+         let size='w500';
          let url_img=uri+size+item.poster_path;
          if(item.poster_path == null){
            url_img='img/nocopertina.png'
